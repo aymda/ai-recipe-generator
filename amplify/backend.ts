71ce307848f28fca4d1706b35d20197c0d0f29a5
aymda <aymda1@gmail.com>
2025-03-21 02:09:04 +0900

    connect to bedrock

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,33 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { data } from "./data/resource";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { auth } from "./auth/resource";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+  "bedrockDS",
+  "https://bedrock-runtime.ap-northeast-1.amazonaws.com",
+  {
+    authorizationConfig: {
+      signingRegion: "ap-northeast-1",
+      signingServiceName: "bedrock",
+    },
+  }
+);
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+  new PolicyStatement({
+    resources: [
+      "arn:aws:bedrock:ap-northeast-1::foundation-model/anthropic.claude-3-5-sonnet-20240620-v1:0",
+    ],
+    actions: ["bedrock:InvokeModel"],
+    
+  })
+);
